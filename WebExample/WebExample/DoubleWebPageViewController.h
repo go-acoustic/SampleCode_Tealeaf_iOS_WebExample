8d67d53937144b3f6d76cfd841411e4478b7d135
Omar Hernandez <ohernandez@us.ibm.com>
2020-01-07 18:59:20 -0800

    EAS-263: Changed from UIWebView to WKWebView.

--- WebExample/WebExample/DoubleWebPageViewController.h
+++ WebExample/WebExample/DoubleWebPageViewController.h
@@ -1,16 +1,19 @@
 //
-//  Licensed Materials - Property of IBM
-//  (C) Copyright IBM Corp. 2017
-//  US Government Users Restricted Rights - Use, duplication or disclosure
-//  restricted by GSA ADP Schedule Contract with IBM Corp.
+// Copyright (C) 2020 Acoustic, L.P. All rights reserved.
+//
+// NOTICE: This file contains material that is confidential and proprietary to
+// Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+// industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+// Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+// prohibited.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface DoubleWebPageViewController : UIViewController <UIWebViewDelegate>
+@interface DoubleWebPageViewController : UIViewController <WKNavigationDelegate, WKScriptMessageHandler>
 
-@property (weak, nonatomic) IBOutlet UIWebView *webView1;
-@property (weak, nonatomic) IBOutlet UIWebView *webView2;
+@property (strong, nonatomic) IBOutlet WKWebView *webView1;
+@property (strong, nonatomic) IBOutlet WKWebView *webView2;
 @property (weak, nonatomic) IBOutlet UILabel *sessionID;
 @property (strong, nonatomic) IBOutlet UIBarButtonItem *leftTopButton;
 
